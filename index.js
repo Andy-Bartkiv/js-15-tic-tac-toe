4b9f09d533d1041d4cb56b8e72b8700fdf00ff84
Andy-W <andy.bartkiv@gmail.com>
2021-04-27 20:17:03 +0300

    TO DO: User Story -> 7 (AI player)

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 // PLAYERS --------------------------------------
 const playerFactory = (mark) => {
     const getMark = () => mark;
+    let _ai = 'person';
     let _canMove = false;
+    const getAI = () => _ai;
+    const toggleAI = () => { _ai = (_ai === 'person') ? 'computer' : 'person'};
     const getMove = () => _canMove;
     const toggleCanMove = () => { _canMove = !(_canMove) };
-    return {getMark, getMove, toggleCanMove}
+    return {getMark, getAI, toggleAI, getMove, toggleCanMove}
 };
 
 // GAME BOARD ----------------------------------
 const gameBoard = ((size = 3) => {
     let board = [
-        ['O', ' ', ' '],
-        [' ', 'X', ' '],
-        [' ', ' ', 'O']
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
     ];
+    let winner = { mark: ' ', line: ''};
+    // DOM caching 
     const grid = document.querySelector('.main-grid');
+    const winnerDiv = document.querySelector('.winner');
+    const playerMarks = [...document.querySelectorAll('.player-mark')];
+    playerMarks[1].classList.add('mark-highlight');
 
+    // Board initialization IIFE
     const init = (() => {
         for (let i = 0; i < size; i++)
             for (let j = 0; j < size; j++) {
@@ -30,83 +39,143 @@ const gameBoard = ((size = 3) => {
     function render() {
         [...grid.children].forEach((el, ind) => {
             el.textContent = board[Math.floor(ind/size)][ind % size];
-            el.classList.remove('hint');
+            el.classList.remove('hint', 'winner-cell');
+            playerMarks.forEach(el => el.classList.toggle('mark-highlight'));
         })
     };
 
     function reset() {
         board = board.map(() => [' ', ' ', ' ']);
+        winner = { mark: ' ', line: ''};
+        winnerDiv.textContent = `TIC - TAC - TOE`;
         render();
     };
 
+    // at hover show if placing mark at cell is possible 
     function hint(id, mark) {
-        // console.log('hint', id)
-        const val = board[id[0]][id[1]];
-        if (val === ' ') {
+        if (id && board[id[0]][id[1]] === ' ') {
             const element = [...grid.children].find(el => el.dataset.id === id);
-            // console.log(element, mark);
             element.classList.add('hint')
             element.textContent = mark;
         }
     }
-
-    function hideHint(id, mark) {
-        // console.log('hide', id)
-        const val = board[id[0]][id[1]];
-        if (val === ' ') {
+    function hideHint(id) {
+        if (board[id[0]][id[1]] === ' ') {
             const element = [...grid.children].find(el => el.dataset.id === id);
-            // console.log(element, mark);
             element.classList.remove('hint')
             element.textContent = ' ';
         }
     }
 
+    // board state update (mark placement / end game determination) on click
     function update(id, mark) {
-        // const id = event.target.dataset.id;
-        // const mark = this.getMark();
-        if (board[id[0]][id[1]] === ' ') {
-            // console.log(id, mark)
+        if (id && board[id[0]][id[1]] === ' ') {
             board[id[0]][id[1]] = mark;
             render();
+            if (gameFlow.getTurn() >= 5) { // 5
+                winner = checkWinner();
+                if (getWinner() !== ' ') {
+                    const line = winner.line.split(',');
+                    [...grid.children].filter(el => line.includes(el.dataset.id)).forEach(el => el.classList.add('winner-cell'))
+                    winnerDiv.textContent = `Player  ${getWinner()}  wins!` ;
+                    playerMarks.forEach(el => el.classList.toggle('mark-highlight'))
+                } 
+                if (gameFlow.getTurn() >= 9) {
+                    playerMarks.forEach(el => el.classList.remove('mark-highlight'))
+                    winnerDiv.textContent = `It is a TIE`;
+                } 
+            }
             return true;    
         }
         return false;
     };
 
-    return {grid, reset, render, update, hint, hideHint};
+    function checkWinner() {
+        const hCheck = () => { // check 3 horizontal lines 
+            const res = [0,1,2].map((y) => (board[y][0] !== ' ') && board[y].every(el => el === board[y][0]))
+                                .indexOf(true);
+            return res >= 0 ? ['0','1','2'].map(el => +res + el).join(',') : '';
+        }
+        const vCheck = () => {  // check 3 vertical lines 
+            const res = [0,1,2].map((x) => (board[0][x] !== ' ') && board.map(row => row[x]).every(el => el === board[0][x]))
+                                .indexOf(true);
+            return res >= 0 ? ['0','1','2'].map(el => el + res).join(',') : '';
+        }
+        const dCheck = () => {  // check 2 diagonal lines 
+            const res = 2 * [0,2].map(z => (board[z][0] !== ' ') && board.map((row, i) => row[Math.abs(z - i)]).every(el => el === board[z][0]))
+                .indexOf(true);
+            return res >= 0 ? ['0','1','2'].map((el, i) => el + Math.abs(res - i)).join(',') : '';  
+        }
+
+        const resCheck = [hCheck(), vCheck(), dCheck()].join(''); // string representing results of all checks
+        const ind = resCheck.split(',')[0];
+        return (resCheck.length > 0)
+            ? { mark: board[ind[0]][ind[1]], line: resCheck }
+            : { mark: ' ', line: '' }
+    }
+
+    function getWinner() {
+        return winner.mark;
+    }
+
+    return {grid, reset, render, update, hint, hideHint, getWinner};
 
 })(); // End of gameBoard Module;
 
 // GAME FLOW ---------------------------------------------------------------------
 const gameFlow = (() => {
-
-    const playerA = playerFactory("X");
-    const playerB = playerFactory("O");
-    const players = [playerA, playerB]
+    const players = [playerFactory("X"), playerFactory("O")];
     players[0].toggleCanMove();
     let player = players.find(pl => pl.getMove());
+    let turnCounter = 1;
+    gameBoard.reset();
 
-    gameBoard.render();
+    // caching DOM
+    const playerTypes = [...document.querySelectorAll('.player-type')]
+    const btnRestart = document.querySelector('.btn-restart');    
 
-    // DOM caching
-    const btnReset = document.querySelector('button[type="reset"]');
-    btnReset.addEventListener('click', gameBoard.reset);
-        
+    // binding Events handlers
+    playerTypes.forEach(el => el.addEventListener('click', togglePlayerType.bind(el)))
+    btnRestart.addEventListener('click', restartGame);        
+
+    // play one turn on click
     gameBoard.grid.addEventListener('click', playOneTurn);
     
-    // gameBoard.grid.addEventListener('dblclick', (event) => event.target.classList.toggle('winner-item'));
-
-    // show hints
+    // show/hide hints
     gameBoard.grid.addEventListener('mouseover', (event) => gameBoard.hint(event.target.dataset.id, player.getMark()));
     [...gameBoard.grid.children].forEach(ch => 
-        ch.addEventListener('mouseleave', (event) => gameBoard.hideHint(event.target.dataset.id, player.getMark())))
-   
+        ch.addEventListener('mouseleave', (event) => gameBoard.hideHint(event.target.dataset.id)));
+    
     function playOneTurn(event) {
-        if (gameBoard.update(event.target.dataset.id, player.getMark())) {
+        if (turnCounter <= 9 && gameBoard.getWinner() === ' ')
+            if (gameBoard.update(event.target.dataset.id, player.getMark())) {
+                players.forEach(pl => pl.toggleCanMove());    
+                player = players.find(pl => pl.getMove());
+                turnCounter += 1;
+            };
+    };
+
+    function restartGame() {
+        if (gameBoard.getWinner() === ' ') {
             players.forEach(pl => pl.toggleCanMove());    
             player = players.find(pl => pl.getMove());
         }
-    }
+        gameBoard.reset();
+        turnCounter = 1;
+    };
 
+    function togglePlayerType(event) {
+        if (event.target.textContent !== players[this.id].getAI()) {
+            players[this.id].toggleAI();
+            [...this.children].forEach(span => span.classList.toggle('md-dark'));
+        }
+        console.log(players.map(el=>el.getAI())) // -------------------------------------- to DELETE
+    };
+
+    function getTurn() {
+        return turnCounter;
+    };
+
+    return {getTurn};
 })();
 
